3638cc7498b552f0e1a2d18e538b2abf894b05ae
andreapesic <a.pesic@sunrock.com>
2025-05-22 08:45:32 +0200

    Add logo 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import promptiersLogo from "./les-promptiers-logo.png";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          src="/les-promptiers-logo.png"
+          src={promptiersLogo}
           alt="Les Promptiers Logo"
           width={180}
           height={180}
